bf2c7046b4a057203c952ceef6417ceb49ab7f97
ArianHussein <302453552@student.rocmondriaan.nl>
2023-03-13 11:27:58 +0100

    Vacationpicker opdracht

---
+++ src/data/countryData.js
@@ -0,0 +1,32 @@
+const countryData = {
+    countries: [
+        {
+            id:1,
+            name: 'USA',
+            capital: 'Washington',
+            cost: 1250,
+            details: 'United States are among the most visited',
+            img: 'wash.jpg',
+            rating:0
+        },
+        {
+            id:2,
+            name: 'Netherlands',
+            capital: 'Amsterdam',
+            cost: 795,
+            details: 'The capital of the Netherlands',
+            img: 'amst.jpg',
+            rating:0
+        },
+        {
+            id:3,
+            name: 'Germany',
+            capital: 'Berlin',
+            cost: 895,
+            details: 'The capital of Germany',
+            img: 'berl.jpg',
+            rating:0
+        },
+    ]
+}
+export default countryData;
